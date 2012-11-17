8f9e06e0fe0ab5602536ccfa0fb0e58666bd45eb
Fileman Development <dev@3960-flap.mdc-crew.net>
2012-11-16 17:35:06 -0700

    DIFROM Inits for the complete languages ISO-636, ISO-636-1 and ISO-636-2 in the language file.

---
+++ routines/fileman/DILAI006.m
@@ -0,0 +1,16 @@
+DILAI006 ; ; 02-NOV-2012
+ ;;22.2;LANGUAGE FILE INITS;;NOV 02, 2012
+ I DSEC F I=1:2 S X=$T(Q+I) Q:X=""  S Y=$E($T(Q+I+1),4,999),X=$E(X,4,999) S:$A(Y)=126 I=I+1,Y=$E(Y,2,999)_$E($T(Q+I+1),5,999) S:$A(Y)=61 Y=$E(Y,2,999) X NO E  S @X=Y
+Q Q
+ ;;^DIC(.85,0,"AUDIT")
+ ;;=
+ ;;^DIC(.85,0,"DD")
+ ;;=^
+ ;;^DIC(.85,0,"DEL")
+ ;;=^
+ ;;^DIC(.85,0,"LAYGO")
+ ;;=^
+ ;;^DIC(.85,0,"RD")
+ ;;=
+ ;;^DIC(.85,0,"WR")
+ ;;=^
