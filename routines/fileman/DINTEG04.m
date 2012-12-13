c7dda4f137703c90834535aa151861639e532c43
Fileman Development <dev@3960-flap.mdc-crew.net>
2012-12-12 19:04:34 -0700

    DINTEG routines added to assure package integrity

---
+++ routines/fileman/DINTEG04.m
@@ -0,0 +1,30 @@
+DINTEG04	;ISC/XTSUMBLD KERNEL - Package checksum checker ;3121212.184524
+	;;0.0;
+	;;7.3;3121212.184524
+	S XT4="I 1",X=$T(+3) W !!,"Checksum routine created on ",$P(X,";",4)," by KERNEL V",$P(X,";",3),!
+CONT	F XT1=1:1 S XT2=$T(ROU+XT1) Q:XT2=""  S X=$P(XT2," ",1),XT3=$P(XT2,";",3) X XT4 I $T W !,X X ^%ZOSF("TEST") S:'$T XT3=0 X:XT3 ^%ZOSF("RSUM") W ?10,$S('XT3:"Routine not in UCI",XT3'=Y:"Calculated "_$C(7)_Y_", off by "_(Y-XT3),1:"ok")
+	;
+	K %1,%2,%3,X,Y,XT1,XT2,XT3,XT4 Q
+ONE	S XT4="I $D(^UTILITY($J,X))",X=$T(+3) W !!,"Checksum routine created on ",$P(X,";",4)," by KERNEL V",$P(X,";",3),!
+	W !,"Check a subset of routines:" K ^UTILITY($J) X ^%ZOSF("RSEL")
+	W ! G CONT
+ROU	;;
+DMLAINI5	;;419539
+DMLAINIT	;;10072774
+DMSQ	;;10102863
+DMSQD	;;8773197
+DMSQE	;;843826
+DMSQF	;;9792265
+DMSQF1	;;8518725
+DMSQF2	;;8370804
+DMSQP	;;2371815
+DMSQP1	;;3565656
+DMSQP2	;;6721470
+DMSQP3	;;11874271
+DMSQP4	;;2151271
+DMSQP5	;;5423594
+DMSQP6	;;10074783
+DMSQS	;;3197279
+DMSQT	;;11672859
+DMSQT1	;;1181888
+DMSQU	;;10591593
