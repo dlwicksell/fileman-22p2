cc26d77498033748991ac7a5f359173a45d1c98c
Fileman Development <dev@3960-flap.mdc-crew.net>
2013-01-23 15:51:06 -0700

    Various fixes dealing with Maximum Global Node Length.
    NO.: 14616                              VERSION: 22.2
      TAG: passim                           ROUTINE NAME OR GLOBAL REF: DINIT0F5
      REPORTER: smh                         DATE CHANGED: JAN 23,2013
      PROGRAMMER: GEORGE TIMSON
      CHANGE: Free Text form Min/Max fields are now free text themselves to allow en
    tries greater than 250, which is hardcoded in DIR
      DOCUMENTATION NEEDED: YES
      DOCUMENTATION NOTES: Document that long nodes are now possible.
      NEW VERSION: <22.2>
     DETAILED TESTING INSTRUCTIONS:
     Set ^DD("STRING_LIMIT") to a large number not to exceed the capacity of
     your database.
    
     In the Screenman Modify File Attributes, make a free text field whose
     length is > 250 characters. It should be accepted. Previously, it
     wouldn't.
    
    NO.: 14615                              VERSION: 22.2
      TAG: H,WITHff                         ROUTINE NAME OR GLOBAL REF: DIR2
      REPORTER: GFT                         DATE CHANGED: JAN 23,2013
      PROGRAMMER: GEORGE TIMSON
      CHANGE: Replace/With prompt now will allow more than 245 characters
      DOCUMENTATION NEEDED: NO              NEW VERSION: <22.2>
     DETAILED TESTING INSTRUCTIONS:
     In a field that can accomodate a lot of data, do a replace/with and put
     in more than 255 characters. It should accept it. Previously, it couldn't.
    
    NO.: 14613                              VERSION: 22.2
      TAG: CHKSUBff                         ROUTINE NAME OR GLOBAL REF: DICATTDM
      REPORTER: SMH                         DATE CHANGED: JAN 23,2013
      PROGRAMMER: GEORGE TIMSON
      CHANGE: Screenman Modify File Atrributes now autosuggests next available node
    if you are above 250 characters         DOCUMENTATION NEEDED: YES
      DOCUMENTATION NOTES: Document that long nodes are now possible
      NEW VERSION: <22.2>
     LONG DESCRIPTION:
     < CHKSUB(X)     ;used as INPUT TRANSFORM for Fields 16 & 76
     ---
     > CHKSUB(X,DISHORT) ;used as INPUT TRANSFORM for Fields 16 (SUBSCRIPT) &
     76 (MUL SUBSCRIPT)  X is the subscript name.  DISHORT says 'don't go
     beyond 250'
     37c38
     <       S M=$$GET^DDSVALF(20.5,"DICATT",1,"","")
     ---
     >  S M=$$GET^DDSVALF(20.5,"DICATT",1,"","") ;'Is this field Multiple?'
     40c41,42
     <       I $G(DICATTLN),$$MAX(DICATTLN,X)>($G(^DD("STRING_LIMIT"),255)-5)
     Q "Too much to store at the '"_X_"' subscript"
     ---
     >  I '$G(DICATTLN) Q 1 ;if we do not have a current length for the field,
     we are OK
     >  S M=$S($G(DISHORT):250,1:$G(^DD("STRING_LIMIT"),255)-5) I
     $$MAX(DICATTLN,X)>M Q "Too much to store at the '"_X_"' subscript"
     DETAILED TESTING INSTRUCTIONS:
     Set ^DD("STRING_LIMIT") to a high value.
    
     Keep making fields until you reach 250 characters full on the same node.
     Make sure that when you get there that
     1. You can still place items in the node.
     2. You are suggested the next node.
    
     You need to test this on both roll and scroll and screenman Modify
     Fileman Attribute forms.

--- routines/fileman/DICATTDM.m
+++ routines/fileman/DICATTDM.m
@@ -1,77 +1,79 @@
-DICATTDM	;GFT/GFT - SUBSCRIPT AND PIECE-POSITION FOR STORAGE OF SINGLE-VALUED DATA IN SCREENMAN ;2013-01-16  11:29 AM
-	;;22.2T1;VA FILEMAN;;Jan 13, 2013
-	;Per VHA Directive 2004-038, this routine should not be modified.
-	;
-SUBDEF	;
-	S Y=$O(^DD(DICATTA,"GL",""),-1)
-	I $$CHKSUB(Y) Q
-NXT	I Y S Y=Y+1 Q
-	F Y=+$O(^DD(DICATTA,"GL","A"),-1):1 Q:'$D(^(Y))
-	Q
-	;
-PIECDEF	;
-	I $E($G(DICATT2N))="K" S Y="E1,245" Q
-	S Y=$$G(16) I Y]"" S Y=$$P(Y)
-	Q
-	;
-P(Y)	;given SUBSCRIPT Y, return PIECE prompt
-	N P,X,%
-	S X=0,%=1,P=0
-PC	S X=$O(^DD(DICATTA,"GL",Y,X)) I X'="" S P=$P(X,",",2),%=$S(%>P:%,1:P+1) G PC
-	I P S %="E"_%_","_(DICATTLN+%-1)
-	E  S %=$O(^(99999),-1)+1
-	Q %
-	;
-SUBHELP	;
-	S Y=$E($G(DICATT2N))="K" D UNED^DDSUTL(17,"DICATTM",3,Y)
-	N X,Y,T
-	S X(1)="Enter name of MUMPS Global subscript where this Field's data will be stored."
-	S X(2)="Already assigned:"
-	S Y="",T=3
-	F  S Y=$O(^DD(DICATTA,"GL",Y)) Q:Y=""  S X(T)=$G(X(T))_$J(Y,9) I $L(X(T))>66 S T=T+1
-	D HLP^DDSUTL(.X)
-	Q
-	;
-CHKSUB(X)	;used as INPUT TRANSFORM for Fields 16 & 76
-	N M
-	S M=$$GET^DDSVALF(20.5,"DICATT",1,"","")
-	I $D(^DD(DICATTA,"GL",X)),M Q "Another Field is already stored at '"_X_"'"
-	I $D(^(X,0)) Q "A multiple field is already stored at '"_X_"'"
-	I $G(DICATTLN),$$MAX(DICATTLN,X)>($G(^DD("STRING_LIMIT"),255)-5) Q "Too much to store at the '"_X_"' subscript"
-	Q 1
-	;
-MAX(L,Y)	;given L=length of new data, Y=subscript name
-	N T,A,DP,N,W
-	S A=DICATTA,DP=DICATTF
-	D MAX^DICATT1 Q T  ;returns maximum length of subscript's data
-	;
-CHKPIEC(P)	;
-	N N,S
-	S S=$$G(16) I S="" Q S  ;must have subscript
-	I P?1"E"1.N1","1.N S N=$P(P,",",2)-$E(P,2,9)+1 G USED:N'<$G(DICATTLN) Q "Can't be less than "_DICATTLN
-	I P>0,P<100,P?.N,+P=P G USED
-	Q ""
-USED	I $D(^DD(DICATTA,"GL",S,P)) Q "Already used for '"_$P(^DD(DICATTA,$O(^(P,0)),0),U)_"'"
-	I P["E",$O(^(0)) Q "Can't store by $EXTRACT in the same subscript with $PIECES"
-	Q 1
-	;
-PIECHELP	;
-	N X,G,Y,P,T
-	S X(1)="Type a number from 1 to 99"
-	S G=$$G(16) Q:G=""
-	I '$D(^DD(DICATTA,"GL",G)) S X(1)=X(1)_" or an $EXTRACT range such as ""E2,4""." Q
-	S X(1)=X(1)_".",X(2)="Currently assigned: ",Y="",T=2
-	F  S Y=$O(^DD(DICATTA,"GL",G,Y)) Q:Y=""  S P=$O(^(Y,0)) I $D(^DD(DICATTA,P,0)) S X(T)=$G(X(T))_$J(Y,7) I $L(X(T))>66 S T=T+1
-	D HLP^DDSUTL(.X)
-	Q
-	;
-POST	;POST-ACTION of Page 3
-	N %
-	S %=$$CHKPIEC($$G(17)) I '% S DDSBR=% K % S %(1)=DDSBR,DDSBR=16 D H(.%)
-	Q
-	;
-H(%)	S %($O(%(""),-1)+1)="$$EOP"
-	D HLP^DDSUTL(.%)
-	Q
-	;
-G(I)	Q $$GET^DDSVALF(I,"DICATTM",3,"","")
+DICATTDM ;GFT/GFT - SUBSCRIPT AND PIECE-POSITION FOR STORAGE OF SINGLE-VALUED DATA IN SCREENMAN ;16JAN2013
+ ;;22.2T1;VA FILEMAN;;Dec 14, 2012
+ ;Per VHA Directive 2004-038, this routine should not be modified.
+ ;
+ ;
+SUBDEF ;EXECUTABLE DEFAULT for FIELD 16 (SUBSCRIPT)
+ S Y=$O(^DD(DICATTA,"GL",""),-1) ;find the highest subscript now used for storage of this File's data
+ I $$CHKSUB(Y,1) Q
+NXT I Y S Y=Y+1 Q  ;get a new subscript
+ F Y=+$O(^DD(DICATTA,"GL","A"),-1):1 Q:'$D(^(Y))
+ Q
+ ;
+PIECDEF ;
+ I $E($G(DICATT2N))="K" S Y="E1,245" Q
+ S Y=$$G(16) I Y]"" S Y=$$P(Y)
+ Q
+ ;
+P(Y) ;given SUBSCRIPT Y, return PIECE prompt
+ N P,X,%
+ S X=0,%=1,P=0
+PC S X=$O(^DD(DICATTA,"GL",Y,X)) I X'="" S P=$P(X,",",2),%=$S(%>P:%,1:P+1) G PC
+ I P S %="E"_%_","_(DICATTLN+%-1)
+ E  S %=$O(^(99999),-1)+1
+ Q %
+ ;
+SUBHELP ;
+ S Y=$E($G(DICATT2N))="K" D UNED^DDSUTL(17,"DICATTM",3,Y)
+ N X,Y,T
+ S X(1)="Enter name of MUMPS Global subscript where this Field's data will be stored."
+ S X(2)="Already assigned:"
+ S Y="",T=3
+ F  S Y=$O(^DD(DICATTA,"GL",Y)) Q:Y=""  S X(T)=$G(X(T))_$J(Y,9) I $L(X(T))>66 S T=T+1
+ D HLP^DDSUTL(.X)
+ Q
+ ;
+CHKSUB(X,DISHORT) ;used as INPUT TRANSFORM for Fields 16 (SUBSCRIPT) & 76 (MUL SUBSCRIPT)  X is the subscript name.  DISHORT says 'don't go beyond 250'
+ N M
+ S M=$$GET^DDSVALF(20.5,"DICATT",1,"","") ;'Is this field Multiple?'
+ I $D(^DD(DICATTA,"GL",X)),M Q "Another Field is already stored at '"_X_"'"
+ I $D(^(X,0)) Q "A multiple field is already stored at '"_X_"'"
+ I '$G(DICATTLN) Q 1 ;if we do not have a current length for the field, we are OK
+ S M=$S($G(DISHORT):250,1:$G(^DD("STRING_LIMIT"),255)-5) I $$MAX(DICATTLN,X)>M Q "Too much to store at the '"_X_"' subscript"
+ Q 1
+ ;
+MAX(L,Y) ;given L=length of new data, Y=subscript name
+ N T,A,DP,N,W
+ S A=DICATTA,DP=DICATTF
+ D MAX^DICATT1 Q T  ;returns maximum length of subscript's data
+ ;
+CHKPIEC(P) ;
+ N N,S
+ S S=$$G(16) I S="" Q S  ;must have subscript
+ I P?1"E"1.N1","1.N S N=$P(P,",",2)-$E(P,2,9)+1 G USED:N'<$G(DICATTLN) Q "Can't be less than "_DICATTLN
+ I P>0,P<100,P?.N,+P=P G USED
+ Q ""
+USED I $D(^DD(DICATTA,"GL",S,P)) Q "Already used for '"_$P(^DD(DICATTA,$O(^(P,0)),0),U)_"'"
+ I P["E",$O(^(0)) Q "Can't store by $EXTRACT in the same subscript with $PIECES"
+ Q 1
+ ;
+PIECHELP ;
+ N X,G,Y,P,T
+ S X(1)="Type a number from 1 to 99"
+ S G=$$G(16) Q:G=""
+ I '$D(^DD(DICATTA,"GL",G)) S X(1)=X(1)_" or an $EXTRACT range such as ""E2,4""." Q
+ S X(1)=X(1)_".",X(2)="Currently assigned: ",Y="",T=2
+ F  S Y=$O(^DD(DICATTA,"GL",G,Y)) Q:Y=""  S P=$O(^(Y,0)) I $D(^DD(DICATTA,P,0)) S X(T)=$G(X(T))_$J(Y,7) I $L(X(T))>66 S T=T+1
+ D HLP^DDSUTL(.X)
+ Q
+ ;
+POST ;POST-ACTION of Page 3
+ N %
+ S %=$$CHKPIEC($$G(17)) I '% S DDSBR=% K % S %(1)=DDSBR,DDSBR=16 D H(.%)
+ Q
+ ;
+H(%) S %($O(%(""),-1)+1)="$$EOP"
+ D HLP^DDSUTL(.%)
+ Q
+ ;
+G(I) Q $$GET^DDSVALF(I,"DICATTM",3,"","")
--- routines/fileman/DIR2.m
+++ routines/fileman/DIR2.m
@@ -1,91 +1,95 @@
-DIR2	;SFISC/XAK-READER (SETUP VARS,REPLACE...WITH) ;3OCT2010
-	;;22.2T1;VA FILEMAN;;Dec 14, 2012
-	;Per VHA Directive 2004-038, this routine should not be modified.
-	K Y,% S U="^"
-	D DIR("A"),DIR("?"),DIR("L"),DIR("B") ;**
-	S %T=$E(DIR(0)),%A=$P(DIR(0),U),%B=$P(DIR(0),U,2),%N=%A'["V"
-	K:$D(DIR("A"))=10 DIR("A") K:$D(DIR("?"))=10 DIR("?")
-	S %W0=$S($D(DIR("?")):DIR("?"),%T'?.AN:"",'$P($T(@(%T_1)),";",5):"",1:$$EZBLD^DIALOG($P($T(@(%T_1)),";",5)))
-	S %A0=$$EZBLD^DIALOG(8041)
-	;**CCO/NI (next line) SPECIFICATION OF READ IS 'DATA DICTIONARY', SO GET FIELD LABEL AND PROPERTIES
-	I %A?.NP1",".ANP S %B1=$P(%A,","),%B2=+$P(%A,",",2) G:'$D(^DD(%B1,%B2,0)) EO S %B3=^(0),%B=$P(%B3,U,2) G:%B EO D:'$D(DIR("B")) DA^DIRQ:$D(DA)#2 S:'$D(DIR("A")) %P=$$LABEL^DIALOGZ(%B1,%B2)_": " S:$P(%B3,U,2)'["R" %A=%A_"O" S %T=1 G NN
-	I "FSYENDLP"'[%T G EO
-	S %B1=$P(%B,":"),%B2=$P(%B,":",2),%B3=$P(%B,":",3)
-	S:'$L(%B2) %B2=$S(%T="D":9991231,%T="F":245,1:999999999999)
-	I %T="F",%B2>245 S %B2=245
-	I %T="Y" S %B=$$EZBLD^DIALOG(7003)
-	I %T="D" S %DT=$P(%B3,"A")_$P(%B3,"A",2)
-	I %T="D",'$D(DIR("?")) S %W0=%W0_$S(%B3["R":$$EZBLD^DIALOG(8043),%B3["T":$$EZBLD^DIALOG(8044),1:"")
-	I %T="D" S %D1=%B1,%D2=%B2 I %B["NOW"!(%B["DT") D NOW^%DTC K %I,%H S DT=X S:%B1["NOW" %B1=% S:%B1["DT" %B1=X S:%B2["NOW" %B2=% S:%B2["DT" %B2=X K %
-	I %T="P" S %B1=$S('%B1:U_%B1,'$D(^DIC(+%B1,0,"GL")):U,1:^("GL")) G EO:%B1=U,EO:'$D(@(%B1_"0)")) I '$D(DIR("A")) S %P=$$EZBLD^DIALOG(8042,$O(^DD(+$P(^(0),U,2),0,"NM",0))) Q
-NN	D:%T="S" S0:%A'["A" Q:$D(%P)
-	S %P="" I %A["A" S:$D(DIR("A")) %P=DIR("A") Q
-	I '$D(DIR("A")) S %P=$$EZBLD^DIALOG($P($T(@%T),";",4)) I %T="D" S %P=%P_$S(%B3["R":$$EZBLD^DIALOG(8043),%B3["T":$$EZBLD^DIALOG(8044),1:"")
-	S:$D(DIR("A")) %P=$S(%T="Y":DIR("A")_"? ",%T="S":$$EZBLD^DIALOG(8045,DIR("A")),1:DIR("A")_": ") I "LND"'[%T Q
-	I $L(%B1) S %P=%P_" ("_$S(%T="D":$$DATE^DIUTL(%B1)_"-"_$$DATE^DIUTL(%B2),1:%B1_"-"_%B2)_")" ;**
-	S %P=%P_$S("?: "[$E(%P,$L(%P)):"",1:":")_" "
-	Q
-S0	S %P=$S($D(DIR("A")):DIR("A")_": ",%A["B":$$EZBLD^DIALOG(8046),1:$$EZBLD^DIALOG($P($T(@%T),";",4)))
-	Q:%A'["B"  S %P=%P_" ("
-	I %B'[":",$O(DIR("C",""))]"" S %I="" F  S %I=$O(DIR("C",%I)) Q:%I=""  D
-	. N Y S Y=$P(DIR("C",%I),":") Q:Y=""
-	. I $D(DIR("S"))#2 X DIR("S") E  Q
-	. S %P=%P_Y_"/"
-	E  F %I=1:1 Q:$P(%B,";",%I,999)=""  D
-	. N Y S Y=$P($P(%B,";",%I),":") Q:Y=""
-	. I $D(DIR("S"))#2 X DIR("S") E  Q
-	. S %P=%P_Y_"/"
-	S %P=$E(%P,1,$L(%P)-(%P?.E1"/"))_"): "
-	Q
-EO	S %T="",Y=-1 Q
-	;
-DIR(DIALA)	;** INSERTS DIALOGS INTO DIR ARRAY
-	N DIALN,DIALP
-	S DIALN=$G(DIR("DIALOG",DIALA))
-	Q:'$D(^DI(.84,0))  Q:+DIALN'=DIALN  Q:'$D(^(DIALN,2))  ;GIVES US A MAKED REFERENCE PRIOR TO CALLING $$EZBLD^DIALOG
-	I $D(DIR("DIALOG",DIALA))>9 M DIALP=DIR("DIALOG",DIALA)
-	K DIR(DIALA) D BLD^DIALOG(DIALN,.DIALP,,"DIR(DIALA)","F")
-	Q
-	;
-	;
-RW	; Replace...With...
-	N %,L S DG=Y S:$D(DTIME)[0 DTIME=999
-A	W:$X>50 ! K DTOUT W $$EZBLD^DIALOG(8047) R X:DTIME E  S DTOUT=1,X=""
-	G B:X="",Q:X?1."^",Q:$E(X)=U&($D(DIRWP)[0)&(Y'[X),Q:X?."?",Q:X="@"
-	I X="END"!(X="end")!(X=$$UP^DILIBF($$EZBLD^DIALOG(7097))) S L=0 D H S:'%&'$D(DTOUT) Y=Y_X G A ;**CCO/NI 'END'
-	I Y[X S D=X,L=$L(X) D H S:'%&'$D(DTOUT) Y=$P(Y,D,1)_X_$P(Y,D,2,999) G A
-	S D=$P(X,"...",1),DH=$F(Y,D) I DH S X=$P(X,"...",2,99),X=$S(X="":$L(Y)+1,1:$F(Y,X,DH)) I X S DH=DH-$L(D)-1,D=X,L=D-DH-1 D H S:'%&'$D(DTOUT) Y=$E(Y,1,DH)_X_$E(Y,D,999) G A
-	W $C(7)," ??" G A
-H	W $$EZBLD^DIALOG(8048) R X:DTIME E  S DTOUT=1,X="",%=0 W $C(7)," ??" Q
-	S %=$L(Y)-L+$L(X)>245 I % W $C(7),$S($L(Y)-L'>245:$$EZBLD^DIALOG(349,($L(Y)-L+$L(X)-245)),X'=U:$$EZBLD^DIALOG(350),1:" ??") Q:$L(Y)-L>245&(X=U)  G H
-	Q:X?.ANP  W $C(7)," ??" G H
-	;
-B	W:$D(DTOUT) $C(7) I DG'=Y S X=Y W !?3 W X I X="" S X="@"
-Q	Q
-	;
-F	;;Enter response: ;8051
-S	;;Enter response: ;8051
-Y	;;Enter Yes or No: ;8052
-E	;;Press RETURN to continue or '^' to exit: ;8053
-N	;;Enter a number;8054
-D	;;Enter a date;8055
-L	;;Enter a list or range of numbers;8056
-P	;;Select: ;8057
-F1	;;;This response can be free text;9031
-S1	;;;Enter a code from the list.;9032
-Y1	;;;Enter either 'Y' or 'N'.;9040
-E1	;;;Enter either RETURN or '^';9033
-N1	;;;This response must be a number;9034
-D1	;;;This response must be a date;9035
-L1	;;;This response must be a list or range, e.g., 1,3,5 or 2-4,8;9036
-	;
-	;#349   String too long by |nuber| character(s)!
-	;#350   String too long! '^' to quit.
-	;#8041  This is a required response...
-	;#8042  Select |1|
-	;#8043  and time
-	;#8044  and optional time
-	;#8045  Enter |1|
-	;#8046  Select one of the following
-	;#8047  Replace
-	;#8048  With
+DIR2 ;SFISC/XAK-READER (SETUP VARS,REPLACE...WITH) ;20JAN2013
+ ;;22.2T1;VA FILEMAN;;Dec 14, 2012
+ ;Per VHA Directive 2004-038, this routine should not be modified.
+ K Y,% S U="^"
+ D DIR("A"),DIR("?"),DIR("L"),DIR("B") ;**
+ S %T=$E(DIR(0)),%A=$P(DIR(0),U),%B=$P(DIR(0),U,2),%N=%A'["V"
+ K:$D(DIR("A"))=10 DIR("A") K:$D(DIR("?"))=10 DIR("?")
+ S %W0=$S($D(DIR("?")):DIR("?"),%T'?.AN:"",'$P($T(@(%T_1)),";",5):"",1:$$EZBLD^DIALOG($P($T(@(%T_1)),";",5)))
+ S %A0=$$EZBLD^DIALOG(8041)
+ ;I %T="t" D SETUP^DIRUD Q  ;if this a user-defined data type read
+ ;**CCO/NI (next line) SPECIFICATION OF READ IS 'DATA DICTIONARY', SO GET FIELD LABEL AND PROPERTIES
+ I %A?.NP1",".ANP S %B1=$P(%A,","),%B2=+$P(%A,",",2) G:'$D(^DD(%B1,%B2,0)) EO S %B3=^(0),%B=$P(%B3,U,2) G:%B EO D:'$D(DIR("B")) DA^DIRQ:$D(DA)#2 S:'$D(DIR("A")) %P=$$LABEL^DIALOGZ(%B1,%B2)_": " S:$P(%B3,U,2)'["R" %A=%A_"O" S %T=1 G NN
+ I "FSYENDLP"'[%T G EO
+ S %B1=$P(%B,":"),%B2=$P(%B,":",2),%B3=$P(%B,":",3)
+ S:'$L(%B2) %B2=$S(%T="D":9991231,%T="F":245,1:999999999999)
+ I %T="F",%B2>245 S %B2=245
+ I %T="Y" S %B=$$EZBLD^DIALOG(7003)
+ I %T="D" S %DT=$P(%B3,"A")_$P(%B3,"A",2)
+ I %T="D",'$D(DIR("?")) S %W0=%W0_$S(%B3["R":$$EZBLD^DIALOG(8043),%B3["T":$$EZBLD^DIALOG(8044),1:"")
+ I %T="D" S %D1=%B1,%D2=%B2 I %B["NOW"!(%B["DT") D NOW^%DTC K %I,%H S DT=X S:%B1["NOW" %B1=% S:%B1["DT" %B1=X S:%B2["NOW" %B2=% S:%B2["DT" %B2=X K %
+ I %T="P" S %B1=$S('%B1:U_%B1,'$D(^DIC(+%B1,0,"GL")):U,1:^("GL")) G EO:%B1=U,EO:'$D(@(%B1_"0)")) I '$D(DIR("A")) S %P=$$EZBLD^DIALOG(8042,$O(^DD(+$P(^(0),U,2),0,"NM",0))) Q
+NN D:%T="S" S0:%A'["A" Q:$D(%P)
+ S %P="" I %A["A" S:$D(DIR("A")) %P=DIR("A") Q
+ I '$D(DIR("A")) S %P=$$EZBLD^DIALOG($P($T(@%T),";",4)) I %T="D" S %P=%P_$S(%B3["R":$$EZBLD^DIALOG(8043),%B3["T":$$EZBLD^DIALOG(8044),1:"")
+ S:$D(DIR("A")) %P=$S(%T="Y":DIR("A")_"? ",%T="S":$$EZBLD^DIALOG(8045,DIR("A")),1:DIR("A")_": ") I "LND"'[%T Q
+ I $L(%B1) S %P=%P_" ("_$S(%T="D":$$DATE^DIUTL(%B1)_"-"_$$DATE^DIUTL(%B2),1:%B1_"-"_%B2)_")" ;**
+ S %P=%P_$S("?: "[$E(%P,$L(%P)):"",1:":")_" "
+ Q
+S0 S %P=$S($D(DIR("A")):DIR("A")_": ",%A["B":$$EZBLD^DIALOG(8046),1:$$EZBLD^DIALOG($P($T(@%T),";",4)))
+ Q:%A'["B"  S %P=%P_" ("
+ I %B'[":",$O(DIR("C",""))]"" S %I="" F  S %I=$O(DIR("C",%I)) Q:%I=""  D
+ . N Y S Y=$P(DIR("C",%I),":") Q:Y=""
+ . I $D(DIR("S"))#2 X DIR("S") E  Q
+ . S %P=%P_Y_"/"
+ E  F %I=1:1 Q:$P(%B,";",%I,999)=""  D
+ . N Y S Y=$P($P(%B,";",%I),":") Q:Y=""
+ . I $D(DIR("S"))#2 X DIR("S") E  Q
+ . S %P=%P_Y_"/"
+ S %P=$E(%P,1,$L(%P)-(%P?.E1"/"))_"): "
+ Q
+EO S %T="",Y=-1 Q
+ ;
+DIR(DIALA) ;** INSERTS DIALOGS INTO DIR ARRAY
+ N DIALN,DIALP
+ S DIALN=$G(DIR("DIALOG",DIALA))
+ Q:'$D(^DI(.84,0))  Q:+DIALN'=DIALN  Q:'$D(^(DIALN,2))  ;GIVES US A MAKED REFERENCE PRIOR TO CALLING $$EZBLD^DIALOG
+ I $D(DIR("DIALOG",DIALA))>9 M DIALP=DIR("DIALOG",DIALA)
+ K DIR(DIALA) D BLD^DIALOG(DIALN,.DIALP,,"DIR(DIALA)","F")
+ Q
+ ;
+ ;
+RW ; Replace...With...
+ N %,L S DG=Y S:$D(DTIME)[0 DTIME=999
+A W:$X>50 ! K DTOUT W $$EZBLD^DIALOG(8047) R X:DTIME E  S DTOUT=1,X=""
+ G B:X="",Q:X?1."^",Q:$E(X)=U&($D(DIRWP)[0)&(Y'[X),Q:X?."?",Q:X="@"
+ I X="END"!(X="end")!(X=$$UP^DILIBF($$EZBLD^DIALOG(7097))) S L=0 D H S:'%&'$D(DTOUT) Y=Y_X G A ;**CCO/NI 'END'
+ I Y[X S D=X,L=$L(X) D H S:'%&'$D(DTOUT) Y=$P(Y,D,1)_X_$P(Y,D,2,999) G A
+ S D=$P(X,"...",1),DH=$F(Y,D) I DH S X=$P(X,"...",2,99),X=$S(X="":$L(Y)+1,1:$F(Y,X,DH)) I X S DH=DH-$L(D)-1,D=X,L=D-DH-1 D H S:'%&'$D(DTOUT) Y=$E(Y,1,DH)_X_$E(Y,D,999) G A
+ W $C(7)," ??" G A
+ ;
+H N DIMAX
+WITH W $$EZBLD^DIALOG(8048) R X:DTIME E  S DTOUT=1,X="",%=0 W $C(7)," ??" Q
+ S DIMAX=$G(^DD("STRING_LIMIT"),255)-10,%=$L(Y)-L+$L(X)>DIMAX
+ I % W $C(7),$S($L(Y)-L'>DIMAX:$$EZBLD^DIALOG(349,($L(Y)-L+$L(X)-DIMAX)),X'=U:$$EZBLD^DIALOG(350),1:" ??") Q:$L(Y)-L>DIMAX&(X=U)  G WITH
+ Q:X?.ANP  W $C(7)," ??" G WITH
+ ;
+B W:$D(DTOUT) $C(7) I DG'=Y S X=Y W !?3 W X I X="" S X="@"
+Q Q
+ ;
+F ;;Enter response: ;8051
+S ;;Enter response: ;8051
+Y ;;Enter Yes or No: ;8052
+E ;;Press RETURN to continue or '^' to exit: ;8053
+N ;;Enter a number;8054
+D ;;Enter a date;8055
+L ;;Enter a list or range of numbers;8056
+P ;;Select: ;8057
+F1 ;;;This response can be free text;9031
+S1 ;;;Enter a code from the list.;9032
+Y1 ;;;Enter either 'Y' or 'N'.;9040
+E1 ;;;Enter either RETURN or '^';9033
+N1 ;;;This response must be a number;9034
+D1 ;;;This response must be a date;9035
+L1 ;;;This response must be a list or range, e.g., 1,3,5 or 2-4,8;9036
+ ;
+ ;#349   String too long by |nuber| character(s)!
+ ;#350   String too long! '^' to quit.
+ ;#8041  This is a required response...
+ ;#8042  Select |1|
+ ;#8043  and time
+ ;#8044  and optional time
+ ;#8045  Enter |1|
+ ;#8046  Select one of the following
+ ;#8047  Replace
+ ;#8048  With
